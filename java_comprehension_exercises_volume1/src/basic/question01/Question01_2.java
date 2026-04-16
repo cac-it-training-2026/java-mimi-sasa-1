ccedfdd623a25f57b1d047fadb024d3fa195edeb
miyusa <miyusa@C010423276.zipangu.cac.co.jp>
2026-04-16 10:34:57 +0900

    Question1完了

--- java_comprehension_exercises_volume1/src/basic/question01/Question01_2.java
+++ java_comprehension_exercises_volume1/src/basic/question01/Question01_2.java
@@ -7,8 +7,18 @@ package basic.question01;
 public class Question01_2 {
 
 	public static void main(String[] args) {
-		
+
 		// TODO:ここに実装
+
+		int applePrice = 100;
+		String stationary = "鉛筆";
+
+		int bananaPrice = applePrice;
+		String writingUtensil = stationary;
+
+		System.out.println("リンゴの値段は" + applePrice + "円です。");
+		System.out.println("バナナの値段は" + bananaPrice + "円です。");
+		System.out.println("この筆記用具は" + writingUtensil + "です。");
 
 	}
 
